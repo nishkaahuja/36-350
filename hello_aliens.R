e0f5286bfdc8213a4298e03a7a10872958d081a3
nishkaahuja <nsahuja@andrew.cmu.edu>
2020-07-30 08:51:45 -0400

    This is the first commit of this R Script file

---
+++ hello_aliens.R
@@ -0,0 +1,4 @@
+Dear aliens,
+
+Before I  introduce Earth, I was wondering which planet or galaxy you have arrived from? Here on Earth, 
+we have expected any alien life forms to visit us from within our galaxy that has 
