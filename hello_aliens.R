943187b82b6836c1ddcfc708ec561d3e3ff6f9fe
nishkaahuja <nsahuja@andrew.cmu.edu>
2020-07-30 08:51:45 -0400

    This is the third commit of this R Script file

---
+++ hello_aliens.R
@@ -0,0 +1,8 @@
+
+cat("Before I  introduce Earth, I was wondering which planet or galaxy you have arrived from and whether or not you have
+a written language where you are from. Here on Earth, we often speculate that alien life forms will visit us from any of 
+the nine planets within our galaxy. We have many different plants, animals, humans, and other forms of living things 
+on our planet. Some of the biggest cities you can visit include Tokyo, Shanghai, New Delhi, and New York, but there are 
+many other exciting places to see during your stay on Earth across our seven continents. Is the planet you have arrived from
+larger or smaller than ours? I am sure many of us on Earth would like to visit your planet! We have sent humans to the moon
+and  the International Space Station, but we are looking forward to exploring other parts of our universe as well.")
